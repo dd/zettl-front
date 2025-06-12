283a8d77b6b7712ad9c22eb0bfafcedc15ddfb89
Dmitry Dobrynin <dd@tovarisch.engineer>
2025-06-13 01:56:11 +0200

    🚧 (chore): Update config, builds and other

--- packages/client/dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld('api', {
-    ping: () => electron_1.ipcRenderer.invoke('ping'),
-    sendNote: (note) => electron_1.ipcRenderer.send('save-note', note),
-});
