05e727fe4cee497c14a0e71b03d50374cd3b2e9b
Dmitry Dobrynin <dd@tovarisch.engineer>
2025-08-12 02:02:04 +0200

    🚧 (chore): Fix win app build

--- packages/win-app/src/preload.ts
+++ packages/win-app/src/preload.ts
@@ -1,4 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron';
+import { type Note } from '@zettl/core/types';
+
 
 contextBridge.exposeInMainWorld('api', {
 	getNotes: () => ipcRenderer.invoke('get-notes'),
